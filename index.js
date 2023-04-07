bd8f86acaa190cb08096367bf733116ff4669549
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-04-07 12:15:40 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate"){
+   return (`This Saturday, I want to ${activity}!`);
+}
+const mondayWork = function(task = "go to the office"){
+    return (`This Monday, I will ${task}.`);
+}
+function wrapAdjective(string="*"){
+    return function (value = "special"){
+        return (`You are ${string}${value}${string}!`);
+    }
+}
+wrapAdjective("||")("a dedicated programmer!");
